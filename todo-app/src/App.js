96d9d7feda160df765f69a783680717f64ebaa5d
woojin <woojinchoi1023@gmail.com>
2023-02-20 23:09:38 +0900

    249

---
+++ todo-app/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+  return <TodoTemplate>todo app을 만들자</TodoTemplate>;
+};
+
+export default App;
